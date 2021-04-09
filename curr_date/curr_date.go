aa5959fd384e4e98e78d2e43ca41171a3d80c2d4
fzk27 <wnz27@icloud.com>
2021-04-09 08:42:55 +0800

    udpate

---
+++ curr_date/curr_date.go
@@ -0,0 +1,20 @@
+/**
+ * @project let-sGo
+ * @Author 27
+ * @Description //TODO
+ * @Date 2021/4/8 23:37 4月
+ **/
+package main
+
+import "fmt"
+
+func main()  {
+	type Date struct {
+		Year int
+		Month int
+		Day int
+	}
+	fmt.Println(Date{})
+
+}
+
