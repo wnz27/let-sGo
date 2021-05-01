48ed769de2bcb67ff1ce5d731f50d333c044c862
fzk27 <feng@littlelights.ai>
2021-05-01 20:35:27 +0800

    update learn go

---
+++ code_content/go-chan/eg9/eg9.go
@@ -0,0 +1,20 @@
+/*
+* Author:  a27
+* Version: 1.0.0
+* Date:    2021/5/1 8:19 下午
+* Description:
+ */
+package main
+// Telnet回音服务器——TCP服务器的基本结构
+
+/*
+Go语言中可以根据实际会话数量创建多个goroutine，并自动的调度它们的处理。
+
+telnet 服务器处理
+ */
+
+func main(){
+
+
+
+}
