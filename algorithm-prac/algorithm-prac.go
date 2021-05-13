7909c49ca80528ac4daa3343288fc1f07aa8cff7
fzk27 <feng@littlelights.ai>
2021-05-13 13:54:14 +0800

    update altorithm

---
+++ algorithm-prac/algorithm-prac.go
@@ -0,0 +1,11 @@
+/*
+* Author:  a27
+* Version: 1.0.0
+* Date:    2021/5/13 1:53 下午
+* Description:
+ */
+package main
+
+func main()  {
+	
+}
---
+++ algorithm-prac/treesss/trees.go
@@ -0,0 +1,19 @@
+/*
+* Author:  a27
+* Version: 1.0.0
+* Date:    2021/5/13 1:53 下午
+* Description:
+ */
+package main
+
+type tree interface {
+	
+}
+
+
+func main()  {
+	
+}
+
+
+
