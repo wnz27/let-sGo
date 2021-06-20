ef0b8e9d019234ace014c6ab974a83488aa9ebcc
fzk27 <feng@littlelights.ai>
2021-06-20 12:26:09 +0800

    fix

--- redis-tst-learn/redis_learn_test.go
+++ redis-tst-learn/redis_learn_test.go
@@ -7,12 +7,9 @@
 package redis_tst_learn
 
 import (
-	"context"
 	"fmt"
-	"testing"
 	"github.com/shirou/gopsutil/mem"
-	"unsafe"
-	"github.com/go-redis/redis/v8"
+	"testing"
 )
 
 func TestRedisSetGetCapacityMemInfo(t *testing.T) {
@@ -31,27 +28,8 @@ func TestRedisSetGetCapacityMemInfo(t *testing.T) {
 	}
 
 	setValue2Redis := func(t *testing.T, value interface{}) {
-		var n2 int64 = 10
-		rdb := redis.NewClient(&redis.Options{
-			Addr:     "localhost:6377",
-			Password: "", // no password set
-			DB:       0,  // use default DB
-		})
-		var ctx = context.Background()
+		setValue()
 
-		err := rdb.Set(ctx, "key1", n2, 0).Err()
-		if err != nil {
-			panic(err)
-		}
-
-		//val, err := rdb.Get(ctx, "key").Result()
-		//if err != nil {
-		//	panic(err)
-		//}
-		//fmt.Println("key", val)
-
-		//rdb.Set(ctx, "t1", , 300)
-		fmt.Printf("\n n2 的类型 %T n2占中的字节数是 %d \n", n2, unsafe.Sizeof(n2))
 	}
 
 	t.Run("before set", func(t *testing.T) {
--- redis-tst-learn/redis_tst.go
+++ redis-tst-learn/redis_tst.go
@@ -6,4 +6,35 @@
  */
 package redis_tst_learn
 
+import (
+	"context"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"unsafe"
+)
 
+func setValue() {
+	var n2 int64 = 10
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6377",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	var ctx = context.Background()
+
+	err := rdb.Set(ctx, "key1", n2, 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+
+	fmt.Printf("\n n2 的类型 %T n2占中的字节数是 %d \n", n2, unsafe.Sizeof(n2))
+
+	//val, err := rdb.Get(ctx, "key").Result()
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println("key", val)
+
+	//rdb.Set(ctx, "t1", , 300)
+}
