324ab16d5057cc0189110839310bcd892b2768a5
fzk27 <feng@littlelights.ai>
2021-07-09 19:45:49 +0800

    update learn ctx

--- code_content/goro_prac/g_learn.go
+++ code_content/goro_prac/g_learn.go
@@ -25,12 +25,16 @@ todo 1、这是最简单的实现控制并发的方式，实现步骤是：
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 	"os/signal"
 	"sync"
 	"syscall"
 	"time"
+	"context"
 )
 
 func type1() {
@@ -152,7 +156,81 @@ recvq 是读操作阻塞在 channel 的 goroutine 列表，sendq 是写操作阻
 
  */
 
+
+/*
+todo Context通常被译作上下文，它是一个比较抽象的概念。在讨论链式调用技术时也经常会提到上下文。
+ 一般理解为程序单元的一个运行状态、现场、快照，而翻译中上下又很好地诠释了其本质，上下则是存在上下层的传递，上会把内容传递给下。
+ 在Go语言中，程序单元也就指的是Goroutine。
+ 每个Goroutine在执行之前，都要先知道程序当前的执行状态，通常将这些执行状态封装在一个Context变量中，传递给要执行的Goroutine中。
+ 上下文则几乎已经成为传递与请求同生存周期变量的标准方法。在网络编程下，当接收到一个网络请求Request，在处理这个Request的goroutine中，
+ 可能需要在当前gorutine继续开启多个新的Goroutine来获取数据与逻辑处理（例如访问数据库、RPC服务等），
+ 即一个请求Request，会需要多个Goroutine中处理。而这些Goroutine可能需要共享Request的一些信息；
+ 同时当Request被取消或者超时的时候，所有从这个Request创建的所有Goroutine也应该被结束。
+ */
+
+type favContextKey string
+
+func type3() {
+	wg := &sync.WaitGroup{}
+	values := []string{"https://www.baidu.com/", "https://www.zhihu.com/"}
+	ctx, cancel := context.WithCancel(context.Background())
+
+	for _, url := range values {
+		wg.Add(1)
+		subCtx := context.WithValue(ctx, favContextKey("url"), url)
+		go reqURL(subCtx, wg)
+	}
+
+	go func() {
+		time.Sleep(time.Second * 3)
+		cancel()
+	}()
+
+	wg.Wait()
+	fmt.Println("exit main goroutine")
+}
+
+func reqURL(ctx context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+	url, _ := ctx.Value(favContextKey("url")).(string)
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Printf("stop getting url:%s\n", url)
+			return
+		default:
+			r, err := http.Get(url)
+			if r.StatusCode == http.StatusOK && err == nil {
+				body, _ := ioutil.ReadAll(r.Body)
+				subCtx := context.WithValue(ctx, favContextKey("resp"), fmt.Sprintf("%s%x", url, md5.Sum(body)))
+				wg.Add(1)
+				go showResp(subCtx, wg)
+			}
+			r.Body.Close()
+			//启动子goroutine是为了不阻塞当前goroutine，这里在实际场景中可以去执行其他逻辑，这里为了方便直接sleep一秒
+			// doSometing()
+			time.Sleep(time.Second * 1)
+		}
+	}
+}
+
+func showResp(ctx context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("stop showing resp")
+			return
+		default:
+			//子goroutine里一般会处理一些IO任务，如读写数据库或者rpc调用，这里为了方便直接把数据打印
+			fmt.Println("printing ", ctx.Value(favContextKey("resp")))
+			time.Sleep(time.Second * 1)
+		}
+	}
+}
+
 func main() {
+	type3()
 }
 
 
