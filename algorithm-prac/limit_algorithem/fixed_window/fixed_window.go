75de3e8d2a3128803cf6a9b5a5aa1ce9d46b659f
fzk27 <feng@littlelights.ai>
2021-07-22 16:27:38 +0800

    udpate timer for go

--- algorithm-prac/limit_algorithem/fixed_window/fixed_window.go
+++ algorithm-prac/limit_algorithem/fixed_window/fixed_window.go
@@ -6,8 +6,47 @@
  */
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
+func tryReqIn() bool {
+	return false
 }
 
-
+func main() {
+	//d := time.Duration(10)
+	//fmt.Println(d)
+	g, ctx := errgroup.WithContext(context.Background())
+	var ticker *time.Ticker = time.NewTicker(1 * time.Second)
+	g.Go(
+		func() error {
+			for t := range ticker.C {
+				fmt.Println("Tick at", t)
+				// Do some thing
+			}
+			exitSignals := []os.Signal{os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGINT} // SIGTERM is POSIX specific
+			sig := make(chan os.Signal, len(exitSignals))
+			signal.Notify(sig, exitSignals...)
+			for {
+				select {
+				case <-ctx.Done():
+					fmt.Println("signal ctx done")
+					return ctx.Err()
+				case <-sig:
+					// do something
+					fmt.Println("exit ticker")
+					ticker.Stop()
+					return nil
+				}
+			}
+		})
+	err := g.Wait() // first error return
+	fmt.Printf("Ticker stopped: %s", err)
+}
