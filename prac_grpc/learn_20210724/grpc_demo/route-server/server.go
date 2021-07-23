77872bdccf79444224982a976f4aba34bf3f04d8
fzk27 <wnz27@icloud.com>
2021-07-24 02:00:34 +0800

    done server

--- prac_grpc/learn_20210724/grpc_demo/route-server/server.go
+++ prac_grpc/learn_20210724/grpc_demo/route-server/server.go
@@ -7,18 +7,39 @@
 package main
 
 import (
-	"google.golang.org/grpc"
-	"net"
+	"context"
 	pb "fzkprac/prac_grpc/learn_20210724/grpc_demo/route"
+	"google.golang.org/grpc"
+	"log"
+	"net"
 )
 
-type RouteGuideServer {
-
+type routeGuideServer struct {
+	pb.UnimplementedRouteGuidServer
 }
 
+func (r routeGuideServer) GetFeature(ctx context.Context, point *pb.Point) (*pb.Feature, error) {
+	return nil, nil
+}
 
-func newServer() *pb.RouteGuidServer {
+func (r routeGuideServer) ListFeatures(rectangle *pb.Rectangle, server pb.RouteGuid_ListFeaturesServer) error {
+	return nil
+}
 
+func (r routeGuideServer) RecordRoute(server pb.RouteGuid_RecordRouteServer) error {
+	return nil
+}
+
+func (r routeGuideServer) Recommend(server pb.RouteGuid_RecommendServer) error {
+	return nil
+}
+
+//func (r routeGuideServer) mustEmbedUnimplementedRouteGuidServer() {
+//	print()
+//}
+
+func newServer() *routeGuideServer {
+	return &routeGuideServer{}
 }
 
 func main() {
@@ -29,5 +50,7 @@ func main() {
 
 	grpcServer := grpc.NewServer()
 	// 把服务注册给 grpc server
-	pb.RegisterRouteGuidServer(grpcServer, )
+	pb.RegisterRouteGuidServer(grpcServer, newServer())
+	log.Fatal(
+		grpcServer.Serve(con))
 }
