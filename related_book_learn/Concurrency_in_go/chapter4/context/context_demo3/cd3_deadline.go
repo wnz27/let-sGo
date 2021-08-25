98f76089b2aacbef0bb39945e383e6df29066b6a
fzk27 <wnz27@icloud.com>
2021-08-26 00:30:57 +0800

    fix comment

--- related_book_learn/Concurrency_in_go/chapter4/context/context_demo3/cd3_deadline.go
+++ related_book_learn/Concurrency_in_go/chapter4/context/context_demo3/cd3_deadline.go
@@ -81,9 +81,8 @@ func genFarewell(ctx context.Context) (string, error) {
 func locale(ctx context.Context) (string, error) {
 	// 这里我们检查我们的context 是否提供了超时时间。如果确实如此，并且我们的系统时钟已经超过截止时间，
 	// 那么我们只会返回context 包中定义的特定错误，即 DeadlineExceeded
-	// Todo 没看懂, 暂时理解会一直check 到没到 deadline
 	fmt.Println("XXXXX")
-	if deadline, ok := ctx.Deadline(); ok {
+	if deadline, ok := ctx.Deadline(); ok {  // Todo 没看懂, 暂时理解 这里这个 ok 会一直check 到没到 deadline
 		fmt.Println("III8IIIIII")
 		if deadline.Sub(time.Now().Add(1 * time.Second)) <= 0 {
 			return "", context.DeadlineExceeded
