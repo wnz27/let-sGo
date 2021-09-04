d31af0d4f40b656bfa17073a941396452f91315d
fzk27 <wnz27@icloud.com>
2021-09-04 21:21:09 +0800

    done simple factory ut

--- design-pattern/content/factory/simple_factory_test.go
+++ design-pattern/content/factory/simple_factory_test.go
@@ -5,3 +5,38 @@
  * @Date 2021/9/4 14:46 9月
  **/
 package factory
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewIRuleConfigParser(t *testing.T) {
+	type args struct {
+		t string
+	}
+	tests := []struct{
+		name string
+		args args
+		want IRuleConfigParser
+	}{
+		{
+			name: "json",
+			args: args{t: "json"},
+			want: jsonRuleConfigParser{},
+		},
+		{
+			name: "yaml",
+			args: args{t: "yaml"},
+			want: yamlRuleConfigParser{},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewIRuleConfigParser(tt.args.t); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewIRuleConfigParser() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
