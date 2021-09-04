942ee6fffe299c750f3220e55b3d977700bdcddb
fzk27 <wnz27@icloud.com>
2021-09-04 23:46:36 +0800

    done abs factory

--- design-pattern/content/factory/abstract_factory.go
+++ design-pattern/content/factory/abstract_factory.go
@@ -5,3 +5,38 @@
  * @Date 2021/9/4 22:47 9月
  **/
 package factory
+
+
+// ISystemConfigParser hh
+type ISystemConfigParser interface {
+	ParseSystem(date []byte)
+}
+
+// jsonSystemConfigParser hh
+type jsonSystemConfigParser struct {
+}
+
+// Parse
+func (j jsonSystemConfigParser) ParseSystem(data []byte) {
+	panic("implement me")
+}
+
+// IConfigParserFactory 工厂方法接口
+type IConfigParserFactory interface {
+	CreateRuleParser() IRuleConfigParser
+	CreateSystemParser() ISystemConfigParser
+}
+
+
+type jsonConfigParserFactory struct {
+}
+
+func(j jsonConfigParserFactory) CreateRuleParser() IRuleConfigParser {
+	return jsonRuleConfigParser{}
+}
+
+func (j jsonConfigParserFactory) CreateSystemParser() ISystemConfigParser {
+	return jsonSystemConfigParser{}
+}
+
+
--- design-pattern/content/factory/abstract_factory_test.go
+++ design-pattern/content/factory/abstract_factory_test.go
@@ -5,3 +5,48 @@
  * @Date 2021/9/4 22:47 9月
  **/
 package factory
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestJsonConfigParserFactory_CreateRuleParser(t *testing.T) {
+	tests := []struct{
+		name string
+		want IRuleConfigParser
+	}{
+		{
+			name: "json",
+			want: jsonRuleConfigParser{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			j := jsonConfigParserFactory{}
+			if got := j.CreateRuleParser(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CreateRuleparser() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestJsonConfigParserFactory_CreateSystemParser(t *testing.T) {
+	tests := []struct{
+		name string
+		want ISystemConfigParser
+	}{
+		{
+			name: "json",
+			want: jsonSystemConfigParser{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			j := jsonConfigParserFactory{}
+			if got := j.CreateSystemParser(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("CreateSystemParser() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
