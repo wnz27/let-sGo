8ab9331f7217e71b7235bc5569ff61182d348150
fzk27 <wnz27@icloud.com>
2021-09-05 23:19:44 +0800

    update

---
+++ design-pattern/content/builder/classic.go
@@ -0,0 +1,104 @@
+/**
+ * @project let-sGo
+ * @Author 27
+ * @Description //TODO
+ * @Date 2021/9/5 22:33 9月
+ **/
+package builder
+
+import "fmt"
+
+const (
+	defaultMaxTotal = 10
+	defaultMaxIdle = 9
+	defaultMinIdle = 1
+)
+
+// ResourcePoolConfig resource pool
+type ResourcePoolConfig struct {
+	name string
+	maxTotal int
+	maxIdle int
+	minIdle int
+}
+
+// ResourcePoolConfigBuilder 用于构建 ResourcePoolConfig
+type ResourcePoolConfigBuilder struct {
+	name string
+	maxTotal int
+	maxIdle int
+	minIdle int
+}
+
+// SetName SetName
+func (b *ResourcePoolConfigBuilder) SetName(name string) error {
+	if name == "" {
+		return fmt.Errorf("name can not be empty")
+	}
+	b.name = name
+	return nil
+}
+
+// SetMinIdle ~
+func (b *ResourcePoolConfigBuilder) SetMinIdle(minIdle int) error {
+	if minIdle < 0 {
+		return fmt.Errorf("min kdle cannot < 0, input: %d", minIdle)
+	}
+	b.minIdle = minIdle
+	return nil
+}
+
+// SetMaxIdle ~
+func (b *ResourcePoolConfigBuilder) SetMaxIdle(maxIdle int) error {
+	if maxIdle < 0 {
+		return fmt.Errorf("max idle cannot < 0, input: %d", maxIdle)
+	}
+	b.maxIdle = maxIdle
+	return nil
+}
+
+// SetMaxTotal SetMaxTotal
+func (b *ResourcePoolConfigBuilder) SetMaxTotal(maxTotal int) error {
+	if maxTotal <= 0 {
+		return fmt.Errorf("max total cannot <= 0, input: %d", maxTotal)
+	}
+	b.maxTotal = maxTotal
+	return nil
+}
+
+
+// Build Build
+func (b *ResourcePoolConfigBuilder) Build() (*ResourcePoolConfig, error) {
+	if b.name == "" {
+		return nil, fmt.Errorf("name can not be empty")
+	}
+
+	// 设置默认值
+	if b.minIdle == 0 {
+		b.minIdle = defaultMinIdle
+	}
+
+	if b.maxIdle == 0 {
+		b.maxIdle = defaultMaxIdle
+	}
+
+	if b.maxTotal == 0 {
+		b.maxTotal = defaultMaxTotal
+	}
+
+	if b.maxTotal < b.maxIdle {
+		return nil, fmt.Errorf("max total(%d) cannot < max idle(%d)", b.maxTotal, b.maxIdle)
+	}
+
+	if b.minIdle > b.maxIdle {
+		return nil, fmt.Errorf("max idle(%d) cannot < min idle(%d)", b.maxIdle, b.minIdle)
+	}
+
+	return &ResourcePoolConfig{
+		name:     b.name,
+		maxTotal: b.maxTotal,
+		maxIdle:  b.maxIdle,
+		minIdle:  b.minIdle,
+	}, nil
+}
+
