8d0eed3d37293d7206ba481b981d7e2caf795fdc
fzk27 <wnz27@icloud.com>
2021-09-06 01:03:35 +0800

    update learn option pattern

---
+++ design-pattern/content/builder/option_pattern_test.go
@@ -0,0 +1,60 @@
+/**
+ * @project let-sGo
+ * @Author 27
+ * @Description //TODO
+ * @Date 2021/9/5 23:29 9月
+ **/
+package builder
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestNewResourcePoolConfig(t *testing.T) {
+	type args struct {
+		name string
+		opts []ResourcePoolConfigOptFunc
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *ResourcePoolConfig
+		wantErr bool
+	}{
+		{
+			name: "name empty",
+			args: args{
+				name: "",
+			},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "success",
+			args: args{
+				name: "test",
+				opts: []ResourcePoolConfigOptFunc{
+					func(option *ResourcePoolConfigOption) {
+						option.minIdle = 2
+					},
+				},
+			},
+			want: &ResourcePoolConfig{
+				name:     "test",
+				maxTotal: 10,
+				maxIdle:  9,
+				minIdle:  2,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := NewResourcePoolConfig(tt.args.name, tt.args.opts...)
+			require.Equalf(t, tt.wantErr, err != nil, "error = %v, wantErr %v", err, tt.wantErr)
+			assert.Equal(t, tt.want, got)
+		})
+	}
+}
