7b719d3e76952f298cfef6b77dcfe1174f9a835d
fzk27 <feng@littlelights.ai>
2021-09-18 14:23:22 +0800

    learn time unix

--- prac_code_content/pre/pre_go_example/e_epoch/epoch.go
+++ prac_code_content/pre/pre_go_example/e_epoch/epoch.go
@@ -6,6 +6,28 @@
  */
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 func main() {
+	now := time.Now()
+	// Use time.Now with Unix or UnixNano to get elapsed time since the Unix epoch in seconds or nanoseconds,
+	// respectively.
+	secs := now.Unix()
+	nanos := now.UnixNano()
+	fmt.Println(now)
+	// Note that there is no UnixMillis,
+	// so to get the milliseconds since epoch you’ll need to manually divide from nanoseconds.
+	millis := nanos / 1000000
+	fmt.Println(secs)
+	fmt.Println(millis)
+	fmt.Println(nanos)
+
+	//  You can also convert integer seconds or nanoseconds since the epoch into the corresponding time.
+	fmt.Println(time.Unix(secs, 0))
+	fmt.Println(time.Unix(0, nanos))
+
 
 }
