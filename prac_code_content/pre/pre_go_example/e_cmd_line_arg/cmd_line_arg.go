f52985cd30c2c609c20943e0ffc117d57af8bb99
fzk27 <wnz27@icloud.com>
2021-09-23 08:42:31 +0800

    done learn cmd_line_arg

--- prac_code_content/pre/pre_go_example/e_cmd_line_arg/cmd_line_arg.go
+++ prac_code_content/pre/pre_go_example/e_cmd_line_arg/cmd_line_arg.go
@@ -6,6 +6,42 @@
  */
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+)
 
+/*
+Command-line arguments are a common way to parameterize execution of programs.
+For example, go run hello.go uses run and hello.go arguments to the go program.
+ */
+
+
+func main() {
+	// os.Args provides access to raw command-line arguments.
+	// Note that the first value in this slice is the path to the program,
+	// and os.Args[1:] holds the arguments to the program.
+	argsWithProg := os.Args
+	argsWithoutProg := os.Args[1:]
+
+	//You can get individual args with normal indexing.
+	arg := os.Args[3]
+
+	fmt.Println(argsWithProg)
+	fmt.Println(argsWithoutProg)
+	fmt.Println(arg)
+
+	/*
+
+	To experiment with command-line arguments it’s best to build a binary with go build first.
+
+	$ go build prac_code_content/pre/pre_go_example/e_cmd_line_arg/cmd_line_arg.go
+	$ ./cmd_line_arg a b c d
+
+	output:
+	[./cmd_line_arg a b c d]
+	[a b c d]
+	c
+
+	*/
 }
