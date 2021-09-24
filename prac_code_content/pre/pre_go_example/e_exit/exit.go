e6911d192586f98a3abefe75682f483b1c66e9ea
fzk27 <feng@littlelights.ai>
2021-09-24 15:09:42 +0800

    learn done exit

--- prac_code_content/pre/pre_go_example/e_exit/exit.go
+++ prac_code_content/pre/pre_go_example/e_exit/exit.go
@@ -6,6 +6,39 @@
  */
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
+/*
+Use os.Exit to immediately exit with a given status.
+ */
+
 func main() {
+	// defers will not be run when using os.Exit,
+	// so this fmt.Println will never be called.
+	defer fmt.Println("!")
+
+	// Exit with status 3.
+	os.Exit(3)
 
 }
+
+/*
+Note that unlike e.g. C, Go does not use an integer return value from main to indicate exit status.
+If you’d like to exit with a non-zero status you should use os.Exit.
+
+If you run exit.go using go run, the exit will be picked up by go and printed.
+$ go run xxxxx.go
+output:
+exit status 3
+
+By building and executing a binary you can see the status in the terminal.
+$ go build xxxxx.go
+$ echo $?
+output:
+3
+
+ */
+
