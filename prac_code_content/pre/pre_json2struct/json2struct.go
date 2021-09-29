191f4bb138c88d5f0e24cfd6c0e963a56899f1da
fzk27 <wnz27@icloud.com>
2021-09-30 00:40:03 +0800

    done json parse hw

--- prac_code_content/pre/pre_json2struct/json2struct.go
+++ prac_code_content/pre/pre_json2struct/json2struct.go
@@ -6,17 +6,65 @@
  */
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
+
 type social struct {
-	facebook string
-	twitter string
+	Facebook string `json:"facebook"`
+	Twitter  string `json:"twitter"`
 }
 
 type User struct {
-	Name string
-	Type string
-	Age int
-	Social social
+	Name   string `json:"name"`
+	Type   string `json:"type"`
+	Age    int    `json:"age"`
+	Social social `json:"social"`
 }
-const jsonFile = "./pre/pre_json2struct/user.json"
+
+type UserList struct {
+	Users []User `json:"users"`
+}
+
+//prac_code_content/pre/pre_json2struct/user.json
+const jsonFile = "./prac_code_content/pre/pre_json2struct/user.json"
+
+func (u *User) Print() string {
+	return fmt.Sprintf(`User Type: %s
+User Age: %d
+User Name: %s
+Facebook Url: %s`, u.Type, u.Age, u.Name, u.Social.Facebook)
+}
+
 func main() {
+	//读取文件
+	jsonBytes := readJsonFileAll(jsonFile)
+
+	// 解析成结构体
+	var users UserList
+	err := json.Unmarshal(jsonBytes, &users)
+	if err != nil {
+		log.Println("parse user list err", err)
+		return
+	}
+
+	for _, u := range users.Users {
+		fmt.Println(u.Print())
+	}
+	fmt.Println(users)
+	//log.Printf("%+v\n", users)
+
+}
+
+func readJsonFileAll(jsonFilePath string) []byte {
+	bytes, err := ioutil.ReadFile(jsonFilePath)
+	if err != nil {
+		log.Println("read json file err", err)
+		return nil
+	}
+	log.Println("Successfully opened users.json")
+	return bytes
 }
