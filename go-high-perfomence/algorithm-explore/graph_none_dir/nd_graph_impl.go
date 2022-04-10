d27728c6512f94657d184799399637f3ecb7613d
wnz27 <wnz27@icloud.com>
2022-04-10 10:51:31 +0800

    feat(impl): none derection graph

---
+++ go-high-perfomence/algorithm-explore/graph_none_dir/nd_graph_impl.go
@@ -0,0 +1,13 @@
+/*
+ * @Author: 27
+ * @LastEditors: 27
+ * @Date: 2022-04-10 10:48:27
+ * @LastEditTime: 2022-04-10 10:51:11
+ * @FilePath: /let-sGo/go-high-perfomence/algorithm-explore/graph_none_dir/nd_graph_impl.go
+ * @description: type some description
+ */
+
+package graph_none_dir
+
+type NDGraph struct {
+}
